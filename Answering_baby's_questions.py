e32c81a8ee0970f1cf5946523dad8ac12ad1927a
sairambas <86200276+sairambas@users.noreply.github.com>
2022-01-17 21:58:27 +0530

    Create Answering_baby's_questions.py

---
+++ Answering_baby's_questions.py
@@ -0,0 +1,7 @@
+questions = ['Why the sun is hot: ','Is the sun really  yellow: ','Is the sun cool then: ']
+index=0
+for question in questions:
+    answer = input(questions[index]).strip().lower()
+    while answer != 'just because':
+        answer  = input('Why? :').lower().strip()
+        index+=1
